e441971360f8fbe615ed238662ee95cac244bc18
letov7482-ui <letov7482@gmail.com>
2026-08-09 14:42:20 +0300

    Delete AetherBoostAIPro/src/main directory

--- AetherBoostAIPro/src/main/java/com/aether/boost/mixin/ChunkCullingMixin.java
+++
@@ -1,9 +0,0 @@
-package com.aether.boost.mixin;
-
-import net.minecraft.client.render.chunk.ChunkBuilder;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(ChunkBuilder.class)
-public class ChunkCullingMixin {
-    // Агрессивное отсечение невидимых чанков
-}
--- AetherBoostAIPro/src/main/java/com/aether/boost/mixin/WaterOptimizerMixin.java
+++
@@ -1,9 +0,0 @@
-package com.aether.boost.mixin;
-
-import net.minecraft.client.render.WorldRenderer;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(WorldRenderer.class)
-public class WaterOptimizerMixin {
-    // Отключение анимации воды на дальних дистанциях
-}
