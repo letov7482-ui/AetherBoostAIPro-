d2d8b0df2edabc1b19c2de6b96dd63093db1e481
letov7482-ui <letov7482@gmail.com>
2026-08-09 15:24:18 +0300

    Update ChunkCullingMixin.java

--- src/main/java/com/aether/boost/mixin/ChunkCullingMixin.java
+++ src/main/java/com/aether/boost/mixin/ChunkCullingMixin.java
@@ -1,20 +1,9 @@
 package com.aether.boost.mixin;
 
 import net.minecraft.client.render.chunk.ChunkBuilder;
-import net.minecraft.util.math.Direction;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ChunkBuilder.BuiltChunk.class)
 public class ChunkCullingMixin {
-
-    @Inject(method = "method_60963", at = @At("HEAD"), cancellable = true, remap = false)
-    private void optimizeVisibilityCheck(Direction from, Direction to, CallbackInfoReturnable<Boolean> cir) {
-        // Агрессивное отсечение: не рендерим чанк, если он полностью за другим
-        if (from == to.getOpposite()) {
-            cir.setReturnValue(false);
-        }
-    }
+    // Агрессивное отсечение чанков будет добавлено в версии 1.1
 }
