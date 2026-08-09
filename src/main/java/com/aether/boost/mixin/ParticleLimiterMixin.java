df03ab17099d112782dea24a7851cfa0a6c7c2eb
letov7482-ui <letov7482@gmail.com>
2026-08-09 15:25:03 +0300

    Update ParticleLimiterMixin.java

--- src/main/java/com/aether/boost/mixin/ParticleLimiterMixin.java
+++ src/main/java/com/aether/boost/mixin/ParticleLimiterMixin.java
@@ -1,34 +1,9 @@
 package com.aether.boost.mixin;
 
 import net.minecraft.client.particle.Particle;
-import net.minecraft.client.render.Camera;
-import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Particle.class)
 public class ParticleLimiterMixin {
-
-    @Inject(method = "method_30835", at = @At("HEAD"), cancellable = true, remap = false)
-    private void limitParticles(MatrixStack matrices, VertexConsumerProvider vertexConsumers, Camera camera, float tickDelta, CallbackInfo ci) {
-        Particle self = (Particle) (Object) this;
-        String className = self.getClass().getSimpleName().toLowerCase();
-
-        // Пропускаем неважные частицы на расстоянии
-        if (className.contains("rain") || className.contains("snow")
-            || className.contains("bubble") || className.contains("torch")
-            || className.contains("dust") || className.contains("drip")) {
-
-            double distX = camera.getPos().x - self.getX();
-            double distY = camera.getPos().y - self.getY();
-            double distZ = camera.getPos().z - self.getZ();
-
-            if (distX * distX + distY * distY + distZ * distZ > 256) { // 16^2 = 256
-                ci.cancel();
-            }
-        }
-    }
+    // Оптимизация частиц будет добавлена в версии 1.1
 }
